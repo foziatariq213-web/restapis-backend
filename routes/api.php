6333d110e9cdc5a8390c73ad2b8943fa2107e0e6
Momina <foziatariq213@gmail.com>
2026-09-22 17:08:25 +0500

    Error Resolved

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use app\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\UserController;
 
 
 Route::get('/ping', function () {
