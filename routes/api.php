432fba8cac83db3f6b8c4a04b77cb5cd68da5629
Momina <foziatariq213@gmail.com>
2026-09-22 17:18:03 +0500

    chore: remove extra blank line (test ruleset fix)

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 
-
 Route::get('/ping', function () {
     return response()->json([
         'status' => 'ok',
